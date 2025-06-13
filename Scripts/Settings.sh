37c7802db1e29efdd6e675f4052eca3355721c87
jimuruxue <1611575262@qq.com>
2025-06-13 20:41:54 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -151,4 +151,5 @@ echo "CONFIG_PACKAGE_luci-lib-taskd=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-lib-xterm=y" >> ./.config
 echo "CONFIG_PACKAGE_taskd=y" >> ./.config
 echo "CONFIG_PACKAGE_quickstart=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-store=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-quickstart=y" >> ./.config
